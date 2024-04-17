b269044c847605dcc2c50df7fe651001e0f053c2
roman <romanzchov@mail.ru>
2024-04-17 12:50:26 +0300

    1

---
+++ src/main/java/com/course2/homeWork/HomeWorkCourseApplication.java
@@ -0,0 +1,13 @@
+package com.course2.homeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkCourseApplication.class, args);
+	}
+
+}
